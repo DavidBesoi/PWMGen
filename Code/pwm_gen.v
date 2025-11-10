0bad19df3a18e19fceaae5d0f559a495a0496d34
David Besoi <david3besoi@gmail.com>
2025-11-10 20:20:13 +0200

    Meam ce greu fu pwm-ul said nobody

--- Code/pwm_gen.v
+++ Code/pwm_gen.v
@@ -12,5 +12,49 @@ module pwm_gen (
     // top facing signals
     output pwm_out
 );
+    reg pwm_out_reg;
+    assign pwm_out = pwm_out_reg;
+
+    reg pwm_out_next;
+
+    wire mode_nealiniat = functions[1]; // 0=Aliniat, 1=Nealiniat
+    wire mode_dreapta   = functions[0]; // 0=Stanga, 1=Dreapta
+
+    // Logica de comparare
+    always @(*) begin
+        // Valoarea default este 0 (inactiv)
+        pwm_out_next = 1'b0;
+
+        if (pwm_en) begin
+            if (mode_nealiniat) begin
+                //Intre compare1 si compare2
+                if ((count_val >= compare1) && (count_val < compare2)) begin
+                    pwm_out_next = 1'b1;
+                end
+            end else begin
+                if (mode_dreapta) begin
+                    // Dreapta:de la compare1 la final
+                    if (count_val >= compare1) begin
+                        pwm_out_next = 1'b1;
+                    end
+                end else begin
+                    // Stanga: de la 0 pana la compare1
+                    if (count_val < compare1) begin
+                        pwm_out_next = 1'b1;
+                    end
+                end
+            end
+        end
+        // Daca pwm_en este 0, ramane valoarea default (1'b0)
+    end
+
+    // Inregistram iesirea pe frontul de ceas
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            pwm_out_reg <= 1'b0;
+        end else begin
+            pwm_out_reg <= pwm_out_next;
+        end
+    end
     
 endmodule
